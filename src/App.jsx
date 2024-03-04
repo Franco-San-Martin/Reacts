510a9b2f7133e5a3b22e54405ee2fae8c046dd9a
Franco San Martin Ruiz <sanmartinfranco04@gmail.com>
2024-03-04 12:53:13 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from './components/NavBar';
+import ItemListContainer from './components/ItemListContainer';
+
+const App = () => {
+    return (
+        <div>
+            <NavBar />
+            <ItemListContainer greeting="Bienvenidos al mercado más grande de Córdoba" />
+        </div>
+    );
+};
+
+export default App;
